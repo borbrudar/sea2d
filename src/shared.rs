266d1a3d678c22d706bea1c43c3606f48c96ed56
Bor Brudar <bor.brudar@gmail.com>
2025-03-04 18:52:19 +0100

    oops

--- src/shared.rs
+++ src/shared.rs
@@ -1,4 +1,4 @@
-pub const LOCAL : &str = "0.0.0.1:6000";
+pub const LOCAL : &str = "0.0.0.0:6000";
 //pub const EXTERNAL : &str = "0.0.0.0:6000";
 //pub const PUBLIC : &str = "188.230.129.8:6000";
 pub const PRIVATE : &str = "192.168.183.149";
