f4e66b549fa52634651bd8c02372afac54e9e6d5
Bor Brudar <bor.brudar@gmail.com>
2025-03-04 18:52:56 +0100

    should work now

--- src/shared.rs
+++ src/shared.rs
@@ -1,7 +1,7 @@
 pub const LOCAL : &str = "0.0.0.0:6000";
 //pub const EXTERNAL : &str = "0.0.0.0:6000";
 //pub const PUBLIC : &str = "188.230.129.8:6000";
-pub const PRIVATE : &str = "192.168.183.149";
+pub const PRIVATE : &str = "192.168.183.149:6000";
 pub const MSG_SIZE: usize = 512;
 pub const SCREEN_WIDTH: u32 = 800;
 pub const SCREEN_HEIGHT: u32 = 600;
