2cc3c9ebb175db2eaccf83ebffc559f0582a612e
Bor Brudar <bor.brudar@gmail.com>
2025-03-10 19:03:09 +0100

    removed player color

--- src/player_packets.rs
+++ src/player_packets.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize,Serialize};
 
-use crate::{animated_texture::AnimatedTexture};
+use crate::animated_texture::AnimatedTexture;
 
 #[derive(Serialize,Deserialize,Debug,Clone,PartialEq)]
 pub enum PlayerPacket{
