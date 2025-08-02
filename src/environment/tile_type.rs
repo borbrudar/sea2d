cf127317473c3efc459756b832eab06933b99c28
Lara Velkavrh <lv31155@student.uni-lj.si>
2025-08-02 20:07:02 +0200

    added more biomes/textures

--- src/environment/tile_type.rs
+++ src/environment/tile_type.rs
@@ -8,6 +8,13 @@ pub enum TileType {
     Wall,
     Stone,
     Inventory,
+    Swamp,
+    Slime,
+    Snow,
+    Ice,
+    Cactus,
+    Cave,
+    Lava,
     //PlayerSpawn,
     Exit(ExitTile),
 }
@@ -29,6 +36,13 @@ impl TileType {
     pub const PLAYER_SPAWN_COLOR: (u8, u8, u8) = (255, 0, 0);
     pub const EXIT_COLOR: (u8, u8, u8) = (64, 58, 171);
     pub const INVENTORY_COLOR: (u8, u8, u8) = (255, 0, 255);
+    pub const SWAMP_COLOR: (u8, u8, u8) = (1, 50, 32);
+    pub const SLIME_COLOR: (u8, u8, u8) = (74, 91, 33);
+    pub const SNOW_COLOR: (u8, u8, u8) = (255, 255, 255);
+    pub const ICE_COLOR: (u8, u8, u8) = (115, 155, 208);
+    pub const CAVE_COLOR: (u8, u8, u8) = (139, 69, 19);
+    pub const LAVA_COLOR: (u8, u8, u8) = (255, 102, 0);
+    pub const CACTUS_COLOR: (u8, u8, u8) = (96, 164, 79);
     // Other colors for each type can be defined here...
 
     pub fn _get_color(&self) -> (u8, u8, u8) {
@@ -41,6 +55,13 @@ impl TileType {
             TileType::Stone => TileType::STONE_COLOR,
             TileType::Wall => TileType::WALL_COLOR,
             TileType::Inventory => TileType::INVENTORY_COLOR,
+            TileType::Swamp => TileType::SWAMP_COLOR,
+            TileType::Slime => TileType::SLIME_COLOR,
+            TileType::Snow => TileType::SNOW_COLOR,
+            TileType::Ice => TileType::ICE_COLOR,
+            TileType::Cave => TileType::CAVE_COLOR,
+            TileType::Lava => TileType::LAVA_COLOR,
+            TileType::Cactus => TileType::CACTUS_COLOR,
             //TileType::PlayerSpawn => TileType::PLAYER_SPAWN_COLOR,
             TileType::Exit(..) => TileType::EXIT_COLOR,
             // Add other variants here...
